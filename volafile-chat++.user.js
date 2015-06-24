92d19a5f91c4054e11fdb1c0bf1629c6ac12d790
Dongmaster <dongmaster@users.noreply.github.com>
2015-06-24 23:59:04 +0200

    Removed duplicate function

--- volafile-chat++.user.js
+++ volafile-chat++.user.js
@@ -82,28 +82,6 @@
 
 }(this));
 
-function notify(message) {
-    var notification = document.createElement('DIV');
-    var notification_message_container = document.createElement('SPAN');
-    var notification_username_container = document.createElement('A');
-    
-    var notification_username = document.createTextNode('System:');
-    var notification_message = document.createTextNode(message);
-    
-    notification.setAttribute("class", "chat_message admin");
-    notification_username_container.setAttribute("class", "username");
-    
-    notification_message_container.appendChild(notification_message);
-    notification_username_container.appendChild(notification_username);
-    
-    notification.appendChild(notification_username_container);
-    notification.appendChild(notification_message_container);
-    
-    document.getElementById('chat_messages').appendChild(notification);
-  
-    document.getElementById('chat_messages').scrollTop = document.getElementById('chat_messages').scrollTop + 30;
-  }
-
 var chat = document.getElementById('chat_input');
 
 chat.addEventListener("keydown", function(e) {
