45d928b855319438f6957249af43bc5124595977
Dongmaster <dongmaster@users.noreply.github.com>
2015-10-14 13:14:18 +0200

    Added Volafile Gallery Fix script. Fixes a bug with the gallery mode where clicking an image opens two tabs.

---
+++ volafile-gallery-fix.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name        Volafile Gallery Fix
+// @namespace   volafile.gallery.fix
+// @description Fixes the bug where two tabs open instead of one in the gallery
+// @include     https://volafile.io/r/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var target = document.querySelector('#gallery_image_wrapper');
+var wot = false;
+
+var observer = new MutationObserver(function (mutations) {
+	mutations.forEach(function (mutation) {
+    var nodes = mutation.addedNodes;
+    
+    if (nodes.length > 0) {
+      init();
+    }
+	});
+});
+
+var config = {
+	childList: true
+};
+
+observer.observe(target, config);
+
+function init() {
+  function fix_click(e) {
+    var gallery_image = document.getElementById("gallery_image");
+    console.log("cock");
+    e.stopPropagation();
+    e.preventDefault();
+
+    var src = gallery_image.src;
+
+    gallery_image.src = "";
+
+    window.open(src, "_blank");
+    gallery_image.src = src;
+  }
+
+  gallery_image.addEventListener("click", fix_click, true);
+  console.log("hue");
+}
