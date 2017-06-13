5aee5f39293de86f174e143ca864da1b9948bccd
dongmaster <dongmaster@users.noreply.github.com>
2017-06-13 19:25:31 +0200

    fixed the highlight script

--- volafile-highlight++.user.js
+++ volafile-highlight++.user.js
@@ -2,48 +2,49 @@
 // @name        Volafile highlight++
 // @namespace   volafile.highlight
 // @description Highlights messages depending on user-defined words.
-// @include     https://volafile.io/r/*
-// @match       https://volafile.io/r/*
-// @version     1
+// @include     https://volafile.(io|org)/r/*
+// @match       https://volafile.(io|org)/r/*
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-// Edit this!
+// Edit the line below.
 var words = [/test/i];
 /* The format is like this:
 ** /word/i
 **
 ** Example:
-** var words = [/dong/i, /myoc/i, /redux/i];
+** var words = [/dong/i, /master/i];
 **
 ** Notes:
 ** Don't forget to include a , symbol between every word.
 ** The power of regex is at the tip of your fingers. /word/i looks for every instance of the word "word" independent on the case, so it would target WORD as well as other combinations.
 **/
-// Edit this!
 
+NodeList.prototype.filter = Array.prototype.filter;
+NodeList.prototype.map = Array.prototype.map;
 
+var observer = new MutationObserver(function(mutations) {
+	mutations.forEach(function(mutation) {
+    var nodes = mutation.addedNodes;
 
+        nodes.forEach(function(node) {
+            var text_nodes = node.querySelectorAll(".chat_text");
 
-
-
-
-var nameNode; 
-var messageNode;
-
-var observer = new MutationObserver(function(mutations){
-	mutations.forEach(function(mutation){
-    
-    nameNode = document.getElementsByClassName('username');
-    nameNodeClasses = nameNode[nameNode.length - 1].parentNode.getAttribute('class');
-    messageNode = document.getElementsByClassName('chat_text');
-    
-    for(var i = 0; i < words.length; i++) {
-      if(words[i].test(messageNode[messageNode.length - 1].innerHTML) && !/highlight/.test(nameNode[nameNode.length - 1].parentNode.getAttribute('class'))) {
-        nameNode[nameNode.length - 1].parentNode.setAttribute('class', nameNodeClasses + ' highlight');
-      }
-    }
-	});
+            words.forEach(function(word) {
+                text_nodes.forEach(function(text_node) {
+                    if (word.test(text_node.innerText)) {
+                        set_highlight(node);
+                    }
+                })
+            })
+        });
+    });
 });
+
+function set_highlight(node) {
+  console.log(node)
+  node.className = node.className + " highlight";
+}
 
 observer.observe(document.querySelector('#chat_messages'), {childList: true});
